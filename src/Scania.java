cab9c822c08dd05a801d8498fef0e3dfdf31050a
Sebulon <sebasjoh@student.chalmers.se>
2019-11-20 14:27:01 +0100

    Lab 1 part 2 del a done

--- src/Scania.java
+++ src/Scania.java
@@ -1,8 +1,11 @@
 import java.awt.*;
 
-public class Scania extends Car {
+public class Scania extends Car implements Flatbed{
 
-    boolean turboOn = false;
+    private boolean turboOn = false;
+    private double degreesOnFlatbed = 0;
+    private final int maxAngle = 70;
+    private final int angleInc = 10;
 
     public Scania(){
         super(2, 350, Color.white, "LBS141");
@@ -17,11 +20,47 @@ public class Scania extends Car {
         turboOn = false;
     }
 
+
+    /**
+     * Depending on the turbo and engine power, changes the (acceleration) speed factor
+     * If the flatbed is raised, returns zero (can't accelerate)
+     * @return Factor on which the speed changes (acceleration factor)
+     */
     @Override
     protected double speedFactor() {
         double turbo = 1;
         if (turboOn);
             turbo = 1.3;
-        return enginePower * 0.01 * turbo;
+        return degreesOnFlatbed == 0 ? enginePower * 0.01 * turbo : 0;
+    }
+
+    /**
+     * Lowers the degrees of the flatbed
+     */
+    @Override
+    public void lowerBed() {
+        if(degreesOnFlatbed >= angleInc){
+            degreesOnFlatbed -= angleInc;
+        }else if(degreesOnFlatbed >= 0){
+            degreesOnFlatbed = 0;
+        }
+    }
+
+    /**
+     * Raises flatbed if stationary
+     */
+    @Override
+    public void raiseBed() {
+        if(currentSpeed == 0){
+            if(degreesOnFlatbed <= maxAngle - angleInc){
+                degreesOnFlatbed += angleInc;
+            }else if(degreesOnFlatbed <= maxAngle){
+                degreesOnFlatbed = maxAngle;
+            }
+        }
+    }
+
+    public double getDegreesOnFlatbed() {
+        return degreesOnFlatbed;
     }
 }
---
+++ test/ScaniaTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScaniaTest {
+
+    Scania scania;
+
+    @Before
+    public void setUp() throws Exception{
+        scania = new Scania();
+    }
+
+    @Test
+    public void speedFactor() {
+        scania.raiseBed();
+        scania.gas(.5);
+        assertEquals(0, scania.getxPos(), 0.0);
+    }
+
+    @Test
+    public void lowerBed() {
+        scania.lowerBed();
+        assertEquals(0, scania.getDegreesOnFlatbed(), 0.0);
+    }
+
+    @Test
+    public void raiseBed() {
+        for (int i = 0; i < 10; i++) {
+            scania.raiseBed();
+        }
+        assertEquals(70, scania.getDegreesOnFlatbed(), 0.0);
+    }
+}
